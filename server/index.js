5a809f612c04f0dd2431c1fc5bb744853e44372e
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-11 12:33:27 +0100

    Ensure sender has access to the private key of the wallet
    Add a signature input box to the send transaction section on the client side, and update the server side to only process a transaction if the transaction details and signature match with the sender (thus proving the sender has access to the private key)

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const web3Accounts = require('web3-eth-accounts');
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,38 +8,44 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+    "0x6C3a074818be2bf372bE9E74A2527cB3DF9771Df": 100,
+    "0x68b05b9adF0420FdE7C50e75B8953a4224b4DCF5": 50,
 };
 
 app.get("/balance/:address", (req, res) => {
-  const { address } = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
+    const {address} = req.params;
+    const balance = balances[address] || 0;
+    res.send({balance});
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+    const {sender, recipient, amount, signature} = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
-  }
+    const transaction = JSON.stringify({
+        sendAmount: amount, recipient: recipient, senderBalance: balances[sender],
+    });
+    const recovered_sender = web3Accounts.recover(transaction, signature)
+
+    if (balances[sender] < amount) {
+        res.status(400).send({message: "Not enough funds!"});
+    } else if (sender === recovered_sender) {
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+        res.send({balance: balances[sender]});
+    } else {
+        res.status(400).send({message: "Signature not valid for this transaction."})
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+    console.log(`Listening on port ${port}!`);
 });
 
 function setInitialBalance(address) {
-  if (!balances[address]) {
-    balances[address] = 0;
-  }
+    if (!balances[address]) {
+        balances[address] = 0;
+    }
 }
